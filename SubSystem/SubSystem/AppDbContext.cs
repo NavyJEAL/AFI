cf83371c4adec80f40cbf8af76906c490f8f5629
Josefine Lundin <id23jln@cs.umu.se>
2026-04-20 20:53:40 +0200

    connected the DB to the project, added a model and DbContext file for clear code

---
+++ SubSystem/SubSystem/AppDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+	{
+	}
+
+	public DbSet<Subscriber> Subscribers { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		modelBuilder.Entity<Subscriber>(entity =>
+		{
+			entity.ToTable("tbl_subscribers");
+			entity.Property(e => e.SubscriberID).HasColumnName("subs_subscriptionnumber");
+			entity.Property(e => e.Personalnumber).HasColumnName("subs_personalnumber");
+			entity.Property(e => e.FirstName).HasColumnName("subs_firstname");
+			entity.Property(e => e.LastName).HasColumnName("subs_lastname");
+			entity.Property(e => e.Address).HasColumnName("subs_address");
+			entity.Property(e => e.ZipCode).HasColumnName("subs_zipcode");
+			entity.Property(e => e.City).HasColumnName("subs_city");
+			entity.Property(e => e.PhoneNumber).HasColumnName("subs_phone");
+		});
+	}
+}
---
+++ SubSystem/SubSystem/Models/Subscriber.cs
@@ -0,0 +1,12 @@
+public class Subscriber
+{
+	public int SubscriberID { get; set; }
+	public required string Personalnumber { get; set; }
+	public required string FirstName { get; set; }
+	public required string LastName { get; set; }
+	public required string Address { get; set; }
+	public required string ZipCode { get; set; }
+	public required string City { get; set; }
+	public required string PhoneNumber { get; set; }
+
+}
