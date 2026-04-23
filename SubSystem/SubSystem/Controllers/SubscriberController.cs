cba9babdb7aa58d1705a2b107c82c49fe83ffe9f
Josefine Lundin <id23jln@cs.umu.se>
2026-04-23 13:31:59 +0200

    added HttpPost, Pull and Deleted methods to the webapi

--- SubSystem/SubSystem/Controllers/SubscriberController.cs
+++ SubSystem/SubSystem/Controllers/SubscriberController.cs
@@ -10,16 +10,58 @@ public class SubscriberController : ControllerBase
 		_context = context;
 	}
 
-	// GET: api/Subscriber/5
+	// GET: api/Subscriber/(id) - Hämta en prenumerant baserat på ID
 	[HttpGet("{id}")]
 	public async Task<ActionResult<Subscriber>> GetSubscriber(int id)
+	{
+		var subscriber = await _context.Subscribers.FindAsync(id);
+
+		if (subscriber == null)
+		{
+			return NotFound();
+		}
+
+		return subscriber;
+	}
+
+	// POST: Skapar en ny prenumerant
+	[HttpPost]
+	public async Task<ActionResult<Subscriber>> CreateSubscriber(Subscriber subscriber)
+	{
+		_context.Subscribers.Add(subscriber);
+		await _context.SaveChangesAsync();
+		return CreatedAtAction(nameof(GetSubscriber), new { id = subscriber.SubscriberID }, subscriber);
+	}
+
+	// PUT: api/Subscriber/(id) - Update existing subscriber
+	[HttpPut("{id}")]
+	public async Task<IActionResult> UpdateSubscriber(int id, Subscriber subscriber)
+	{
+		if (id != subscriber.SubscriberID)
+		{
+			return BadRequest();
+		}
+
+		_context.Entry(subscriber).State = EntityState.Modified;
+		await _context.SaveChangesAsync();
+
+		return NoContent();
+	}
+
+	// DELETE: Raderar en prenumerant baserat på ID
+	[HttpDelete("{id}")]
+	public async Task<IActionResult> DeleteSubscriber(int id)
 	{
 		var subscriber = await _context.Subscribers.FindAsync(id);
 		if (subscriber == null)
 		{
 			return NotFound();
 		}
-		return subscriber;
+
+		_context.Subscribers.Remove(subscriber);
+		await _context.SaveChangesAsync();
+
+		return NoContent();
 	}
 
 }
